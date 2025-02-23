5eb12a6b040e4fcbc6cbde921cd1a50a66a115b1
aditya-xq <adityavivek1998@gmail.com>
2025-02-23 18:39:49 +0530

    direct import of css

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-  import { dev } from '$app/environment'
   import { onMount } from 'svelte'
   
-  // Import local styles in dev, package styles in prod
-  dev ? import('../../../dist/alt.min.css') : import('altcss/alt.min.css')
+  // import '../../../dist/alt.min.css'
+  import 'altcss/alt.min.css'
 
   let isDarkMode = $state(false)
   onMount(() => {
